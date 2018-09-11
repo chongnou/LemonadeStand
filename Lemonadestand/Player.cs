7bc91b7a750e3181e05efeca5e1c19909b813d06
chong <39102393+chongnou@users.noreply.github.com>
2018-09-10 21:53:43 -0500

    added player class

---
+++ Lemonadestand/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonadestand
+{
+    class Player
+    {
+        // Member Variables
+        public string name;
+        public string nameInput;
+
+        //Constructor
+        public Player()
+        {
+
+        }
+        
+        //Methods
+        public void GetPlayerName()
+        {
+            User_Interface.DisplayMessage("Enter Your Name: ");
+
+            nameInput = User_Interface.GetUserInput();
+            name = nameInput.ToUpper();
+        }
+    }
+}

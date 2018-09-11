477ec301dd60da70411bf41f554ed6f2e21d71c5
chong <39102393+chongnou@users.noreply.github.com>
2018-09-10 21:55:58 -0500

    added weather class

---
+++ Lemonadestand/Weather.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonadestand
+{
+    class Weather
+    {
+        // Member Variables
+        Random rnd;
+        public int result;
+
+
+        //Constructor
+        public Weather(Random rnd)
+        {
+            this.rnd = rnd;
+        }
+
+
+        //Member Methods
+
+
+        //public override void WeatherForcast()
+        public int WeatherForcast()
+        {
+            result = rnd.Next(1, 4);
+            return result;
+        }
+    }
+}
